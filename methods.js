b9419b0a5bf100ac404fa5a8b73130842d161393
johnalain <98930620+johnalain@users.noreply.github.com>
2025-09-08 10:48:11 +0300

    add javascript methods
    add javascript methods

---
+++ methods.js
@@ -0,0 +1,197 @@
+// const items = [
+//         { name: 'bike', price: 100 },
+//         { name: 'tv', price: 200 },
+//         { name: 'album', price: 10 },
+//         { name: 'book', price: 5 },
+//         { name: 'phone', price: 500 },
+//         { name: 'computer', price: 1000 },
+//         {name:'keyboard',price:25}
+// ]
+// const filteredItems = items.filter((items) => {
+//         return items.price<=100
+// })
+// const foundItem = items.forEach((item) => {
+//        console.log(item.price)
+// })
+// const total = items.reduce((currentTotal,item) => {
+//         // console.log(item.price)
+//         return item.price + currentTotal
+// },0)
+// console.log(total)
+// const hasInexpensiveItems = items.some((item) => {
+//         // console.log(item.price)
+//         return item.price <= 100
+// })
+//  console.log(hasInexpensiveItems)
+//  const hasInexpensiveItems = items.some((item) => {
+//         // console.log(item.price)
+//         return item.price <= 100
+// })
+//  console.log(hasInexpensiveItems)
+
+
+// const itemNames = items.map((item) => {
+//         return item.price
+// })
+// console.log(foundItem)
+// console.log(filteredItems)
+// const items =[1,2,3,4,5]
+// const includesTwo = items.includes(2)
+// console.log(includesTwo)
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// [
+//   { name: 'bike', price: 100 },
+//   { name: 'album', price: 10 },
+//   { name: 'book', price: 5 },
+//   { name: 'keyboard', price: 25 }
+// ]
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// [
+//   { name: 'bike', price: 100 },
+//   { name: 'tv', price: 200 },
+//   { name: 'album', price: 10 },
+//   { name: 'book', price: 5 },
+//   { name: 'phone', price: 500 },
+//   { name: 'computer', price: 1000 },
+//   { name: 'keyboard', price: 25 }
+// ]
+// [
+//   { name: 'bike', price: 100 },
+//   { name: 'album', price: 10 },
+//   { name: 'book', price: 5 },
+//   { name: 'keyboard', price: 25 }
+// ]
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// Waiting for the debugger to disconnect...
+// /Users/michelkadi/javascript methods/methods.js:16
+// console.log(itemsitcontains)
+//             ^
+
+// ReferenceError: itemsitcontains is not defined
+//     at Object.<anonymous> (/Users/michelkadi/javascript methods/methods.js:16:13)
+//     at Module._compile (node:internal/modules/cjs/loader:1358:14)
+//     at Module._extensions..js (node:internal/modules/cjs/loader:1416:10)
+//     at Module.load (node:internal/modules/cjs/loader:1208:32)
+//     at Module._load (node:internal/modules/cjs/loader:1024:12)
+//     at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:174:12)
+//     at node:internal/main/run_main_module:28:49
+
+// Node.js v20.14.0
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// [
+//   'bike',     'tv',
+//   'album',    'book',
+//   'phone',    'computer',
+//   'keyboard'
+// ]
+// Waiting for the debugger to disconnect...
+// /Users/michelkadi/javascript methods/methods.js:17
+// console.log(filteredItems)
+//             ^
+
+// ReferenceError: filteredItems is not defined
+//     at Object.<anonymous> (/Users/michelkadi/javascript methods/methods.js:17:13)
+//     at Module._compile (node:internal/modules/cjs/loader:1358:14)
+//     at Module._extensions..js (node:internal/modules/cjs/loader:1416:10)
+//     at Module.load (node:internal/modules/cjs/loader:1208:32)
+//     at Module._load (node:internal/modules/cjs/loader:1024:12)
+//     at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:174:12)
+//     at node:internal/main/run_main_module:28:49
+
+// Node.js v20.14.0
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// [
+//   'bike',     'tv',
+//   'album',    'book',
+//   'phone',    'computer',
+//   'keyboard'
+// ]
+// Waiting for the debugger to disconnect...
+// /Users/michelkadi/javascript methods/methods.js:17
+// console.log(filteredItems)
+//             ^
+
+// ReferenceError: filteredItems is not defined
+//     at Object.<anonymous> (/Users/michelkadi/javascript methods/methods.js:17:13)
+//     at Module._compile (node:internal/modules/cjs/loader:1358:14)
+//     at Module._extensions..js (node:internal/modules/cjs/loader:1416:10)
+//     at Module.load (node:internal/modules/cjs/loader:1208:32)
+//     at Module._load (node:internal/modules/cjs/loader:1024:12)
+//     at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:174:12)
+//     at node:internal/main/run_main_module:28:49
+
+// Node.js v20.14.0
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// [
+//   'bike',     'tv',
+//   'album',    'book',
+//   'phone',    'computer',
+//   'keyboard'
+// ]
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// [
+//   100, 200,   10,
+//     5, 500, 1000,
+//    25
+// ]
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// { name: 'book', price: 5 }
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// bike
+// tv
+// album
+// book
+// phone
+// computer
+// keyboard
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// 100
+// 200
+// 10
+// 5
+// 500
+// 1000
+// 25
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// true
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// false
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// true
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// undefined[object Object]
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// 1840
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods node methods.js
+// Debugger attached.
+// true
+// Waiting for the debugger to disconnect...
+// ➜  javascript methods includesTwo
+
